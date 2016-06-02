5acd3853221d9f7e73746d0db6217ebc80d9b7c8
André Willik Valenti <awvalenti@gmail.com>
2016-06-02 13:36:43 -0300

    Externando CSS e JS

---
+++ javascript/dom/dom-exemplo-5-evento-onclick.js
@@ -0,0 +1,21 @@
+var qtdItens = 0;
+
+function adicionarLi() {
+  var novoLi = document.createElement('li');
+  novoLi.textContent = qtdItens++;
+  document.getElementById('minha-lista').appendChild(novoLi);
+  atualizarQtdeItens();
+}
+
+function removerLi() {
+  if (qtdItens > 0) {
+    var ul = document.getElementById('minha-lista');
+    ul.removeChild(ul.lastElementChild);
+    --qtdItens;
+    atualizarQtdeItens();
+  }
+}
+
+function atualizarQtdeItens() {
+  document.getElementById('quantidade-itens').textContent = qtdItens;
+}
