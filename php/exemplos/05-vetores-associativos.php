69260c8b65dd4b313bc71370f07691e93d038e6a
Andre Willik Valenti <awvalenti@gmail.com>
2017-08-28 12:51:25 -0300

    Explicando melhor vetor associativo

--- php/exemplos/05-vetores-associativos.php
+++ php/exemplos/05-vetores-associativos.php
@@ -1,5 +1,8 @@
 <?php
 
+// Vetor associativo permite associar strings a outros valores.
+// A chave tem que ser do tipo string; o valor pode ser de qualquer tipo.
+
 // Cria vetor
 $vetor = array('dez' => 10, 'vinte' => 20, 'trinta' => 30);
 echo $vetor['dez'] . "\n";
